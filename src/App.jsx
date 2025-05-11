90cc1665bb89109031e1db13ce4a00cf34ab1119
ashutosh2712 <ashu00619@gmail.com>
2025-05-11 16:51:05 +0530

    Initial react app setup using vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Chatbot Frontend</h1>
+
+      <h2>Welcome to the Chatbot Frontend!</h2>
+    </div>
+  );
+}
+
+export default App;
